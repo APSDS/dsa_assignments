0e3692cdbcdf2ae8c873fc830bf512d7ba9ce425
APSDS <145789388+APSDS@users.noreply.github.com>
2026-09-19 20:24:49 +0530

    Update accesspoin.c

--- Assignment_04/Pointers/accesspoin.c
+++ Assignment_04/Pointers/accesspoin.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-
-int main()
-{
-    int arr[5] = {1, 2, 3, 4, 5};
-
-    printf("after access:");
-    printf("%d", *(arr + 2));
-
-    return 0;
+#include<stdio.h>
+int main(){
+     int arr[10]={10,20,30,40,50,60,70,80,90};
+     int ptr*=arr;
+     int ind;
+     printf("enter the index which you want to print");
+     scanf("%d" &ind) ;
+     if(ind>0 && ind<10){
+            printf("%d",*(ptr+ind));
+     }
+     else{
+            printf("index not found");
+     }
+     return 0;
 }
